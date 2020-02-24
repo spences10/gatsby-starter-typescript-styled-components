51c258fae357dee2d096ca8a9435ba1aca1e733e
spences10 <spences10apps@gmail.com>
2020-02-24 21:00:24 +0000

    add gatsby typescript plugin and tsconfig

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,9 @@ module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-typescript`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -14,8 +17,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
