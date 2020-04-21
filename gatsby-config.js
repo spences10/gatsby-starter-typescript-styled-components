6de76ca09ea661b64746c498abd9b5b2a405823a
spences10 <spences10apps@gmail.com>
2020-04-21 11:00:47 +0100

    update siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,19 @@
+const siteMetadata = {
+  title: `Gatsby TypeScript starter`,
+  description: `Gatsby TypeScript starter with styled-components. Built with the Gatsby default starter.`,
+  authorName: `Scott Spence`,
+  image: `/gatsby-astronaut.png`,
+  siteUrl: `https://testurl.com`,
+  siteLanguage: `en-GB`,
+  siteLocale: `en_gb`,
+  twitterUsername: `@spences10`,
+  favicon: `./static/favicon.png`,
+  backgroundColor: `#639`,
+  themeColor: `#639`,
+}
+
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
+  siteMetadata,
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
@@ -23,17 +33,22 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: siteMetadata.backgroundColor,
+        theme_color: siteMetadata.themeColor,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: siteMetadata.favicon, // This path is relative to the root of the site.
       },
     },
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [`cambay\:400,700`, `arvo\:400,700`, `ubuntu mono\:400,700`],
+        fonts: [
+          `cambay\:400,700`,
+          `arvo\:400,700`,
+          `ubuntu mono\:400,700`,
+        ],
       },
+      display: "swap",
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
